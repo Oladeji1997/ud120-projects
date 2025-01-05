812ec2ce8dd1ff4f82f154b9c344e02e72ca1579
Oladeji1997 <eniolaoladeji2@gmail.com>
2025-01-04 20:26:50 -0500

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,40 +12,23 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("/content/ud120-projects/tools")
 from email_preprocess import preprocess
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
-##############################################################
-# Enter Your Code Here
 clf = GaussianNB()
-
-
-##############################################################
-
-##############################################################
-'''
-You Will be Required to record time for Training and Predicting 
-The Code Given on Udacity Website is in Python-2
-The Following Code is Python-3 version of the same code
-'''
 
 t0 = time()
 # # < your clf.fit() line of code >
 clf.fit(features_train, labels_train)
 print("Training Time:", round(time()-t0, 3), "s")
 
-# t0 = time()
+t0 = time()
 # # < your clf.predict() line of code >
 pred = clf.predict(features_test)
-# print("Predicting Time:", round(time()-t0, 3), "s")
+print("Predicting Time:", round(time()-t0, 3), "s")
 
 accuracy = accuracy_score(labels_test, pred)
 print("Accuracy:", accuracy)
